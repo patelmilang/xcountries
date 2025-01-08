259985349b3a4b89dd735d5af8a46d1cfb47f6eb
milan <patelmilang@gmail.com>
2025-01-08 14:48:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+ 
+import "./App.css";
+ import Flag from './component/Flag' 
+
+function App() {
+
+
+  return (
+    <div>
+       
+        <Flag />
+       
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Flag.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const Flag = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      const result = await axios.get(
+        "https://xcountries-backend.azurewebsites.net/all"
+      );
+      setData(result.data);
+    };
+    fetchCountries();
+  }, []);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        gap: "10px",
+        flexWrap: "wrap",
+      }}
+    >
+       
+      {data.map((item) => {
+      return (  <CountryCard key={item.abbr} flag={item.flag} name={item.name} />)
+      })}
+    </div>
+  );
+};
+
+const CountryCard = ({ flag, name }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignContent: "center",
+        alignItems: "center",
+        justifyContent: "center",
+        border: "1px solid gray",
+        flexDirection: "column",
+        padding: "10px",
+        width: "200px",
+        height: "200px",
+        textAlign:"center",
+        borderRadius:"5px"
+      }}
+    >
+      <img src={flag} alt={name} style={{ height: "100px", width: "100px" }} />
+      <h3>{name}</h3>
+    </div>
+  );
+};
+export default Flag;
